6766f684bc1e046186c5b8ac05a7b2f26658d61b
svetaegress <svetaegress@gmail.com>
2017-03-13 00:03:54 +0800

    Lesson 1

---
+++ hello2.rb
@@ -0,0 +1,6 @@
+puts "Как тебя зовут?"
+name = gets.chomp
+puts "#{name}, привет!"
+puts "В каком году ты родился?"
+year = gets.chomp
+puts "#{name}, привет! Тебе примерно #{2015 - year.to_i} лет."
