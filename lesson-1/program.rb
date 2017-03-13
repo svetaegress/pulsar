25afaf9bbc42d4129a3d3c29eb5a64b8b7fff379
svetaegress <svetaegress@gmail.com>
2017-03-13 18:24:48 +0800

    add ideal weight program

---
+++ lesson-1/program.rb
@@ -0,0 +1,12 @@
+puts "Как тебя зовут?"
+name = gets.chomp
+puts "Какой у тебя рост?"
+
+height = gets.chomp. to_i
+ideal_weight = height - 110
+
+if ideal_weight < 0
+	puts "Ваш вес уже идеальный!"
+else
+  puts "#{name}, ваш вес идеальный и равен #{ideal_weight}"
+end
