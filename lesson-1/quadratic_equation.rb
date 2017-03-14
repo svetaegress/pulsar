9ac0671072175152ed1f2d9b5369d1bf920794cd
svetaegress <svetaegress@gmail.com>
2017-03-14 12:30:16 +0800

    add quadratic equation program

---
+++ lesson-1/quadratic_equation.rb
@@ -0,0 +1,21 @@
+print "Введите коэффициент a"
+a = gets.to_f
+
+print "Введите коэффициент b"
+b = gets.to_f
+
+print "Введите коэффициент c"
+c = gets.to_f
+
+d = b ** 2 - 4 * a * c
+if d > 0 
+  sqrtd = Math. sqrtd(d)
+  x1 = ( -b + sqrtd ) / ( 2 * a)
+  x2 = ( -b - sqrtd ) / ( 2 * a)
+  puts "Дискриминант = #{d}, корни #{x1} и #{x2}"
+elsif d == 0
+  x1 = -b / (2 * a)
+  puts "Дискриминант = #{d}, корни #{x1}"
+elsif d < 0 
+  puts "Дискриминант = #{d}, корней нет"
+end
