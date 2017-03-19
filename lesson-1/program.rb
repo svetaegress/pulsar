654cf7140ac56d3803e6b9e57f84f954ade982fc
svetaegress <svetaegress@gmail.com>
2017-03-19 21:59:45 +0800

    add program.rb

--- lesson-1/program.rb
+++ lesson-1/program.rb
@@ -6,7 +6,7 @@ height = gets.chomp. to_i
 ideal_weight = height - 110
 
 if ideal_weight < 0
-	puts "Ваш вес уже идеальный!"
+  puts "Ваш вес уже идеальный!"
 else
   puts "#{name}, ваш вес идеальный и равен #{ideal_weight}"
 end
